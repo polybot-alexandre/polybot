24f7ddc0b30b74397b19c42eff12cb129a1b34d7
polybot-alexandre <xandyandrade@gmail.com>
2025-05-19 14:09:01 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ cloudinary.config(
     cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
     api_key=os.getenv("CLOUDINARY_API_KEY"),
     api_secret=os.getenv("CLOUDINARY_API_SECRET")
+)
 
 user_language_choice = {}
 
